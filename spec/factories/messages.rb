b303c276effd959389782eb265e5c0a35597d299
松岡諒 <ryomatsu0120@gmail.com>
2018-04-07 15:08:24 +0900

    テスト作成

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
+    image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image.png'))
     user
     group
   end
