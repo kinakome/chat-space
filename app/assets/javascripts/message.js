f9ffae5d0c9a8905372345f97edbdb08cf2abe3a
松岡諒 <ryomatsu0120@gmail.com>
2018-05-15 17:09:56 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,73 @@
-$(function(){
-	function buildHTML(message){
-	    var html =  `<div class="message">
-	   					<div class="upper-message">
-	   						<div class="upper-message__user-name">
-	    					${message.user_name}
-	    					</div>
-	    					<div class="upper-message__date">
-	    					${message.created_at}
-	    					</div>
-	    				</div>
-	   					<div class="lower-meesage">
-	    					<p class="lower-message__content">
-	    						${message.content}
-	    					</p>
-	    				</div>
-	   				</div>`
-	    return html;
-	    }
+$(document).on('turbolinks:load', function() {
+	$(function(){
+		function buildHTML(message){
+		    var html =  `<div class="message" data-message-id="${message.id}">
+		   					<div class="upper-message">
+		   						<div class="upper-message__user-name">
+		    					${message.user_name}
+		    					</div>
+		    					<div class="upper-message__date">
+		    					${message.created_at}
+		    					</div>
+		    				</div>
+		   					<div class="lower-meesage">
+		    					<p class="lower-message__content">
+		    						${message.content}
+		    					</p>
+		    				</div>
+		   				</div>`
+		    return html;
+		    }
 
-	$('#new_message').on('submit', function(e){
-	e.preventDefault();
-	var formData = new FormData(this);
-	var url = $(this).attr('action')
+	    var interval = setInterval(function() {
+	      	if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+	      		var message_id = $('.message:last').data('message-id');
+			    $.ajax({
+			      	url: location.href.json,
+			      	dataType: 'json',
+			      	type: 'GET',
+			      	data: {
+        					message: { id: message_id }
+      						}
+			    })
+			    .done(function(data) {
+			    	console.log(data);
+			      	var id = $('.message').data('messageId');
+			      	var insertHTML = '';
+			     	data.forEach(function(message) {
+				          	insertHTML += buildHTML(message);
+			      	});
+			      	$('.messages').append(insertHTML);
+			    })
+			    .fail(function(data) {
+			      	console.log('自動更新に失敗しました');
+			    });
+	  		} else {
+	    		clearInterval(interval);
+	   	}} , 5000 );
 
-		$.ajax({
-		url: url,
-		type: 'POST',
-		data: formData,
-		dataType: 'json',
-		processData: false,
-		contentType: false
-		})
-    .done(function(data){
-    	var html = buildHTML(data);
-      	$('.messages').append(html).animate({scrollTop: $('.messages')[0].scrollHeight}, '500');
-     	$('#new_message')[0].reset();
-     	$('input').prop('disabled', false);
-    })
-    .fail(function() {
-      alert('メッセージが入力されていません');
-    })
+		$('#new_message').on('submit', function(e){
+		e.preventDefault();
+		var formData = new FormData(this);
+		var url = $(this).attr('action')
+
+			$.ajax({
+			url: url,
+			type: 'POST',
+			data: formData,
+			dataType: 'json',
+			processData: false,
+			contentType: false
+			})
+	    .done(function(data){
+	    	var html = buildHTML(data);
+	      	$('.messages').append(html).animate({scrollTop: $('.messages')[0].scrollHeight}, '500');
+	     	$('#new_message')[0].reset();
+	     	$('input').prop('disabled', false);
+	    })
+	    .fail(function() {
+	      alert('メッセージが入力されていません');
+	    })
+		});
 	});
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,12 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
+    @group = Group.find(params[:group_id])
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json {@new_message = @messages.where('id > ?', params[:message][:id]) }
+    end
   end
 
   def create
